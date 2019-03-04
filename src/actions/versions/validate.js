9c54fd71257c5f10b059008a57e1d6fc20025720
smoogie <lukasz.pawlowski.inf@gmail.com>
2019-03-04 08:17:56 +0100

    Add API Gateway
    simple API gateway

--- src/actions/versions/validate.js
+++ src/actions/versions/validate.js
@@ -1,6 +1,9 @@
 exports.handler = async (event, context) => {
     const response = {
-        "success": true
+        'statusCode': 200,
+        'body': JSON.stringify({
+            accepted: true
+        })
     }
     return response;
 };
