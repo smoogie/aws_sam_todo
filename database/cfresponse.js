1f7541678d747785cb9c68f07cd67b8099d90cb1
smoogie <lukasz.pawlowski.inf@gmail.com>
2019-03-06 23:03:40 +0100

    testing with logs
    still some errors but no logs, so change to async

--- database/cfresponse.js
+++ database/cfresponse.js
@@ -1,10 +1,30 @@
 const https = require("https");
 const url = require("url");
 
+function httpRequest(params, postData) {
+    return new Promise((resolve, reject) => {
+        var req = https.request(params, (res) => {
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                reject(new Error('statusCode=' + res.statusCode));
+            }
+            res.on('end', () => {
+                resolve(res);
+            });
+        });
+        req.on('error', (err) => {
+            reject(err);
+        });
+        if (postData) {
+            req.write(postData);
+        }
+        req.end();
+    });
+}
+
 module.exports = {
     SUCCESS: "SUCCESS",
     FAILED: "FAILED",
-    send(event, context, responseStatus, responseData) {
+    async send(event, context, responseStatus, responseData) {
         const responseBody = JSON.stringify({
             Status: responseStatus,
             Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
@@ -17,28 +37,22 @@ module.exports = {
         });
         const responseUrl = url.parse(event.ResponseURL);
         const options = {
-            hostname: parsedUrl.hostname,
+            hostname: responseUrl.hostname,
             port: 443,
-            path: parsedUrl.path,
+            path: responseUrl.path,
             method: "PUT",
             headers: {
                 "content-type": "",
                 "content-length": responseBody.length
             }
         };
-
-        const request = https.request(options, (response) => {
+        try {
+            response = await httpRequest(options, responseBody);
             console.log("Status code response from CF: " + response.statusCode);
             console.log("Status message response from CF: " + response.statusMessage);
-            context.done();
-        });
-
-        request.on("error", (error) => {
-            console.log("Error sending CF response: " + error);
-            context.done();
-        });
-
-        request.write(responseBody);
-        request.end();
+        } catch (error) {
+            console.log(error);
+        }
+        context.done();
     }
 }
