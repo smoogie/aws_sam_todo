d932707eb191d00c5b9cf6a3a61433450230b39d
smoogie <lukasz.pawlowski.inf@gmail.com>
2019-03-07 15:41:02 +0100

    working migration on update

--- database/migration.js
+++ database/migration.js
@@ -3,13 +3,17 @@ const cfResponse = require('cfresponse');
 
 exports.handler = async (event, context) => {
     umzug = umzugInitializer.init('./migrations', 'migrations');
+    if (event.RequestType === 'Delete') {
+        let cfResult = await cfResponse.send(event, context, cfResponse.SUCCESS, {});
+        return true;
+    }
     try {
         migrations = await umzug.up();
         console.log(migrations);
         let cfResult = await cfResponse.send(event, context, cfResponse.SUCCESS, {});
     } catch (error) {
         console.log(error);
-        cfResponse.send(event, context, cfResponse.FAILED, {});
+        let cfResult = await cfResponse.send(event, context, cfResponse.FAILED, {});
         return false;
     }
     return true;
