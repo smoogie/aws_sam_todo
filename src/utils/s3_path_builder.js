4e04dc76823cd1504411b790e124c2311e78090b
smoogie <lukasz.pawlowski.inf@gmail.com>
2019-03-12 15:07:09 +0100

    s3 events

--- src/utils/s3_path_builder.js
+++ src/utils/s3_path_builder.js
@@ -1,7 +1,3 @@
 exports.bucket = process.env.S3_PUBLIC_DATA;
-exports.buildItemId = (itemType, itemId, ext, type="origin") => {
-    return `${itemType}/${itemId}/${type}_${Date.now()}.${ext}`;
-};
-exports.buildUrl = (itemPath) => {
-    return `https://s3.${process.env.DEPLOYMENT_REGION}.amazonaws.com/${process.env.S3_PUBLIC_DATA}/${itemPath}`
-};
+exports.buildItemId = (itemType, itemId, ext, type = 'origin') => `${type}/${itemType}/${itemId}/${Date.now()}.${ext}`;
+exports.buildUrl = itemPath => `https://s3.${process.env.DEPLOYMENT_REGION}.amazonaws.com/${process.env.S3_PUBLIC_DATA}/${itemPath}`;
