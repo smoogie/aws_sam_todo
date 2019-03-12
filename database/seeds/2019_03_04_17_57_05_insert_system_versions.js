ee6e0624d994a4002a6ddc255b20ee1815899599
smoogie <lukasz.pawlowski.inf@gmail.com>
2019-03-12 15:07:47 +0100

    eslint

--- database/seeds/2019_03_04_17_57_05_insert_system_versions.js
+++ database/seeds/2019_03_04_17_57_05_insert_system_versions.js
@@ -2,14 +2,14 @@ const Promise = require('bluebird');
 const Sequelize = require('sequelize');
 
 module.exports = {
-    up: function(queryInterface, DataTypes) {
-        return queryInterface.bulkInsert('system_versions', [{
-            weight: 1,
-            system: 'ios',
-            version: 'test',
-            min: true
-        }]);
-    },
-    down: function(queryInterface, DataTypes) {
-    }
+  up(queryInterface, DataTypes) {
+    return queryInterface.bulkInsert('system_versions', [{
+      weight: 1,
+      system: 'ios',
+      version: 'test',
+      min: true
+    }]);
+  },
+  down(queryInterface, DataTypes) {
+  }
 };
